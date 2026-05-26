b9f12bf0c17a9aa2cf7f510a02e74aee0a12cb3a
UK Study Buddy <ukstudybuddyguide@gmail.com>
2026-05-26 23:52:20 +0300

    whitenoise

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from decouple import config, Csv
 import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,6 +20,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -66,6 +66,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
